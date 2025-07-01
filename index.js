ed716a43ff473cae116e72056512dbf949695554
Victor-Alex-Moreira-Gouveia <Victor.Alex.Moreira.Gouveia@gmail.com>
2025-06-30 22:00:16 -0300

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/api/data', (req, res) => {
   // Handle GET request to /api/data
@@ -10,17 +10,7 @@ app.get('/api/data', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  // Serve the index.html file from the backend directory
-  if (res.statusCode === 404) {
-    res.status(404).send('Not Found');
-    return;
-  }
-
-  // Ensure the index.html file exists in the same directory as this script
-  else if (res.statusCode === 200) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  }
-
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
